de91a50682b85d7692a3abb1ae632c9f6f02f249
rumaysak <125397561+rumaysak@users.noreply.github.com>
2023-04-25 14:48:26 -0500

    Add files via upload

---
+++ ps12p1.py
@@ -0,0 +1,28 @@
+
+#define the object
+class Employee:
+  # use pass to keep empty for now
+  def _init_(self, first, last, pay)
+  self, first = first
+  self, last = last
+  self, pay = pay
+  self, email = first + ',' + last + @company.com
+  #self, rate = 0.0
+
+def bonus(self, rate):
+  b = float(rate) * float(self, pay)
+  return b
+#main + execution begins here
+# instantiate the object
+emp11 = Employee('Rumaysa', 'Khan', 70000.00) 
+
+#use the object
+#object syntax in object method
+print(emp11, email)
+print(emp11, first)
+print(emp11, last)
+print(emp11, pay)
+print(emp11, bonus(0.10))
+print(emp11, bonus(0.20))
+
+
---
+++ ps12p2.py
@@ -0,0 +1,27 @@
+
+#define the object
+class student:
+  # use pass to keep empty for now
+  def _init_(first, last, dc)
+  self, first = first
+  self, last = last
+  self, district code = dc
+  self, name = first + ',' + last + @company.com
+  #self, rate = 0.0
+
+def pch(self, rate):
+  b = float(rate) * float(self, dc)
+  return b
+#main + execution begins here
+# instantiate the object
+stu22 = student('Rumaysa', 'Khan', 70000.00) 
+
+#use the object
+#object syntax in object method
+print(stu22, email)
+print(stu22, first)
+print(stu22, last)
+print(stu22, dc)
+print(stu22, pch(0.250.00))
+print(stu22, pch(500.00))
+
